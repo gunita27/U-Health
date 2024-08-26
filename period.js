2f64270664f96c6034ab97d3e7f45d7ce2a71172
priya21052005 <pv0408288@gmail.com>
2024-08-26 07:59:20 +0530

    modify calculator

--- period.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-        flatpickr('#custom-date-input', {
-            dateFormat: "d F l",  // day month weekday
-            onChange: function (selectedDates, dateStr, instance) {
-                // Optional: Add any additional logic if needed
-                console.log("Selected date:", dateStr);
-            }
-        });
-    });
